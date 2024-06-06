dc93a92af7281f32818122326604abb186eb44aa
bgpkrishna123 <bgpkrishna123@gmail.com>
2024-06-06 18:42:29 +0530

    cors update

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,9 @@ const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+     origin: '*'
+}));
 
 app.use("/users", UserRouter);
 app.use("/books", bookRouter);
